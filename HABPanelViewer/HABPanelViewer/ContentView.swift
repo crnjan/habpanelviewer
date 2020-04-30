83d4df615a9c9631cd8b23f2538dc5f9d7ba3107
Boris Krivonog <boris.krivonog@inova.si>
2020-04-30 20:34:00 +0200

    Initial commit

---
+++ HABPanelViewer/HABPanelViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HABPanelViewer
+//
+//  Created by Boris Krivonog on 30/04/2020.
+//  Copyright © 2020 Boris Krivonog. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
